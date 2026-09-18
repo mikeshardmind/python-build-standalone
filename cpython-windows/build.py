ca6b7a55c6b8d04c3768b75e59b51c8670258fa1
Jonathan J. Helmus <jjhelmus@gmail.com>
2026-09-18 14:05:52 -0500

    Set MSBuild from Visual Studio environment PATH. (#1271)
    Use the MSBuild executable that is on PATH after activation of the
    Visual Studio environment.
    
    Previously a hardcoded path was used that always selected a 32-bit x86
    MSBuild executable.

--- cpython-windows/build.py
+++ cpython-windows/build.py
@@ -281,12 +281,6 @@ def find_vs_path(path, msvc_version):
     return p
 
 
-def find_msbuild(msvc_version):
-    return find_vs_path(
-        pathlib.Path("MSBuild") / "Current" / "Bin" / "MSBuild.exe", msvc_version
-    )
-
-
 def find_vcvarsall_path(msvc_version):
     """Find path to vcvarsall.bat"""
     return find_vs_path(
@@ -1436,7 +1430,6 @@ def build_cpython(
     target_triple: str,
     arch: str,
     build_options: str,
-    msvc_version: str,
     platform_toolset: str,
     windows_sdk_version: str,
     openssl_archive,
@@ -1448,8 +1441,11 @@ def build_cpython(
     pgo = "pgo" in parsed_build_options
     freethreaded = "freethreaded" in parsed_build_options
 
-    msbuild = find_msbuild(msvc_version)
-    log("found MSBuild at %s" % msbuild)
+    msbuild_path = shutil.which("MSBuild.exe")
+    if msbuild_path is None:
+        raise FileNotFoundError("MSBuild.exe was not found on PATH")
+    msbuild = pathlib.Path(msbuild_path)
+    log("using MSBuild from PATH: %s" % msbuild)
 
     # The python.props file keys off MSBUILD, so it needs to be set.
     os.environ["MSBUILD"] = str(msbuild)
@@ -2145,7 +2141,6 @@ def main() -> None:
             target_triple,
             arch,
             build_options=build_options,
-            msvc_version=args.vs,
             platform_toolset=platform_toolset,
             windows_sdk_version=args.windows_sdk_version,
             openssl_archive=openssl_archive,
